63c4a35f7cb73c8a41ab7c8f8dc89c06bbbe4698
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-09-10 12:53:05 +0530

    add getallusers functionality

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,11 @@
+import User from "../models/user.model.js";
+
+export const getAllUsers = async (req, res) => {
+    try {
+        const loggedInUserId = req.user._id;
+        const remainingUsers = await User.find({_id: {$ne: loggedInUserId}}).select("-password");
+        return res.status(200).json({message: "users fetched successfully", users: remainingUsers})
+    } catch (error) {
+        return res.status(500).json({message: error.message || "something went wrong"})
+    }
+}
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,9 @@
 import express from "express"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {getAllUsers} from "../controllers/user.controller.js"
 
 const router = express.Router();
+
+router.get("/", verifyJWT, getAllUsers);
 
 export default router;
